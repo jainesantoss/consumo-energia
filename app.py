b708f1a38a043af8624312ae86a7b25c5d6865be
Jaíne Santos <jaine.santoseduarda@gmail.com>
2026-09-07 20:45:06 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-aparelho=input("Digite o nome do aparelho:")|
+aparelho=input("Digite o nome do aparelho:")
 potencia=float(input("Digite a potência em watts:"))
 horas_dia=float(input("Digite as horas:")) 
 consumo_mensal=(potencia*horas_dia*30)/1000
