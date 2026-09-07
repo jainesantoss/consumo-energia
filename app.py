0c6a6e1fe9324fbb69565b17fc95ec59b27ee05c
Jaíne Santos <jaine.santoseduarda@gmail.com>
2026-09-07 20:33:07 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
-aparelho=input("geladeira")
-potencia=float(input("digite a potência em watts:"))
-horas_dia=float(input("digite as horas:"))
+aparelho=input("Digite o nome do aparelho:")|
+potencia=float(input("Digite a potência em watts:"))
+horas_dia=float(input("Digite as horas:")) 
 consumo_mensal=(potencia*horas_dia*30)/1000
-print(consumo_mensal)
+print(f"Aparelho: {aparelho}")
+print(f"Consumo estimado: {consumo_mensal} kWh/mês")
